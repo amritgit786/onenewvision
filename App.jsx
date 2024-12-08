aa3dcb70c6004f141f10927dd1ba95a35e716592
amritgit786 <133605015+amritgit786@users.noreply.github.com>
2024-12-08 13:43:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="container">
+      <div className="items items1">Header</div>
+      <div className="items items2">Menu</div>
+      <div className="items items3">Box-1</div>
+      <div className="items items4">Box-2</div>
+      <div className="items items5">Content</div>
+      <div className="items items6">Sidebar</div>
+      <div className="items items7">Footer</div>
+    </div>
+  );
+}
